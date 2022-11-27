c351677c3199330b17c313a76906f2c84562b1fe
Pixeldeck <philipmir@hotmail.com>
2022-11-27 12:26:59 +0100

    update3

--- app/src/main/java/com/example/cardgame/Card.kt
+++ app/src/main/java/com/example/cardgame/Card.kt
@@ -1,6 +1,6 @@
 package com.example.cardgame
 
-class Card(var suitNnumber: String, var graphic: Int, var index: Int) {
+class Card(var suitNnumber: String, var graphic: Int, var value: Int) {
 
 }
 
--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.cardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
 
     lateinit var cardImageView: ImageView
     lateinit var scoreTextView: TextView
@@ -18,67 +20,83 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //hiding the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_main)
 
 
-        cardImageView = findViewById(R.id.cardImageView)
-        scoreTextView = findViewById(R.id.scoreTextView)
-        upImageButton = findViewById(R.id.upImageButton)
-        downImageButton = findViewById(R.id.downImageButton)
 
-        cardImageView.setImageResource(R.drawable.card_back)
+            cardImageView = findViewById(R.id.cardImageView)
+            scoreTextView = findViewById(R.id.scoreTextView)
+            upImageButton = findViewById(R.id.upImageButton)
+            downImageButton = findViewById(R.id.downImageButton)
 
-
-
-
-
-        upImageButton.setOnClickListener {
-            checkScoreUp()
-        }
-
-        downImageButton.setOnClickListener {
-            checkScoreDown()
-
-        }
+            cardImageView.setImageResource(R.drawable.card_back)
 
 
 
+
+
+            upImageButton.setOnClickListener {
+                fullDeck.pickCard()
+                checkScoreUp()
+
+
+
+            }
+
+            downImageButton.setOnClickListener {
+                fullDeck.pickCard()
+                checkScoreDown()
+
+
+            }
 
 
     }
 
     fun scorePrint () {
         var score = scoreCount
-        scoreTextView.text = "$score"
+        scoreTextView.text = score.toString()
     }
 
-    fun endOfPile() {
-        if (fullDeck.fullDeck.size.equals(1)) {
-
-        cardImageView.setImageResource(R.drawable.black_joker)}
-
-
-
-        }
+//    fun endOfPile() {
+//
+//        if (fullDeck.fullDeck.size.equals(1)) {
+//
+//            fullDeck.replayGame()
+//
+//
+//
+//        }
+//    }
 
     fun checkScoreUp () {
-        fullDeck.pickCard()
-        if (fullDeck.removeCard.index < fullDeck.currentCard2.index){
+
+
+        if (fullDeck.removeCard.value < fullDeck.currentCard2.value){
+            scoreCount++
+        }
+        cardImageView.setImageResource(fullDeck.currentCard2.graphic)
+        scorePrint()
+        }
+
+
+    fun checkScoreDown () {
+
+        if (fullDeck.removeCard.value > fullDeck.currentCard2.value){
             scoreCount++
         }
         cardImageView.setImageResource(fullDeck.currentCard2.graphic)
         scorePrint()
     }
 
-    fun checkScoreDown () {
-        fullDeck.pickCard()
-        if (fullDeck.removeCard.index > fullDeck.currentCard2.index){
-            scoreCount++
-        }
-        cardImageView.setImageResource(fullDeck.currentCard2.graphic)
-        scorePrint()
-    }
 }
+
+
+
+
 
 
 
